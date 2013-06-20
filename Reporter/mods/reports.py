e87b504dc762012dc66020ba4adf5a7487aea637
AB <none@none>
2013-06-20 18:37:29 +0400

    Add guests cards to reports
    --HG--
    branch : new-features

--- Reporter/mods/reports.py
+++ Reporter/mods/reports.py
@@ -32,7 +32,7 @@ def get_companies_list():
         print(e)
         exit()
     c = conn.cursor()
-    c.execute('''SELECT CM.Name FROM Companies AS CM ORDER BY CM.Name''')
+    c.execute("""SELECT CM.Name FROM Companies AS CM ORDER BY CM.Name""")
     companies_list = [row.Name for row in c]
     c.close()
     return companies_list
@@ -50,12 +50,15 @@ def reports_company(company, report_path, rd_start, rd_end):
     daily_tariffs = {'_Постоянные карты': [[datetime.time(0, 0, 0), datetime.time(0, 15, 0), 0],
                      [datetime.time(0, 15, 0), datetime.time(7, 0, 0), 250],
                      [datetime.time(7, 0, 0), datetime.time(23, 59, 59), 0]],
+                     'Разовый': [[datetime.time(0, 0, 0), datetime.time(0, 15, 0), 0],
+                     [datetime.time(0, 15, 0), datetime.time(7, 0, 0), 250],
+                     [datetime.time(7, 0, 0), datetime.time(23, 59, 59), 0]],
                      '_Всё бесплатно':    [[datetime.time(0, 0, 0), datetime.time(23, 59, 59), 0]]}
     date_frm = '%Y-%m-%d %H:%M:%S'
 
     conn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=parktime35;UID=sa;PWD=123')
     c = conn.cursor()
-    sql = '''SELECT CM.Name,
+    sqll = """SELECT CM.Name,
                  CS.Name,
                  UPPER(SUBSTRING(MASTER.DBO.FN_VARBINTOHEXSTR(CR.ID), 3, 8)),
                  TRF.Name AS Tariff,
@@ -69,8 +72,39 @@ def reports_company(company, report_path, rd_start, rd_end):
                                                  LEFT JOIN Tariffs AS TRF ON CR.TariffID = TRF.ID
                                                  LEFT JOIN Customers AS CS ON CR.CustomerID = CS.ID
                                                 ) ON CM.ID = CR.CompanyID
-                 WHERE CR.ID IS NOT NULL AND CM.Name = ?'''  # temporary block empty CR.ID
-    c.execute(sql, (rd_start, rd_end, company))
+                 WHERE CR.ID IS NOT NULL AND CM.Name = ?  -- temporary block empty CR.ID
+                 ORDER BY T.TimeExit ASC"""
+    sql = """SELECT CM.Name,
+                 CS.Name,
+                 UPPER(SUBSTRING(MASTER.DBO.FN_VARBINTOHEXSTR(CR.ID), 3, 8)),
+                 TRF.Name AS Tariff,
+                 T.TimeEntry, T.TimeExit
+                 FROM Companies AS CM LEFT JOIN (
+                                                 Cards AS CR
+                                                 INNER JOIN Transactions AS T ON CR.ID = T.CardID
+                                                                              AND T.Type = 2 AND T.TimeEntry < T.TimeExit
+                                                                              AND T.TimeEntry > '2000-01-01'
+                                                                              AND T.TimeExit BETWEEN ? AND ?
+                                                 LEFT JOIN Tariffs AS TRF ON CR.TariffID = TRF.ID
+                                                 LEFT JOIN Customers AS CS ON CR.CustomerID = CS.ID
+                                                ) ON CM.ID = CR.CompanyID
+                 WHERE CR.ID IS NOT NULL AND CM.Name = ?  -- temporary block empty CR.ID
+            UNION
+              SELECT CM.Name,
+                 'Разовая карта',
+                 UPPER(SUBSTRING(MASTER.DBO.FN_VARBINTOHEXSTR(CR.CardID), 3, 8)),
+                 'Разовый',
+                 T.TimeEntry, T.TimeExit
+                 FROM Companies AS CM LEFT JOIN (
+                                                 gcards.dbo.gcards AS CR
+                                                 INNER JOIN Transactions AS T ON CR.CardID = T.CardID
+                                                                              AND T.Type = 2 AND T.TimeEntry < T.TimeExit
+                                                                              AND T.TimeEntry > '2013-06-20'
+                                                                              AND T.TimeExit BETWEEN ? AND ?
+                                                ) ON CM.ID = CR.CompanyID
+                 WHERE CR.CardID IS NOT NULL AND CM.Name = ?
+            ORDER BY T.TimeExit ASC"""
+    c.execute(sql, (rd_start, rd_end, company, rd_start, rd_end, company))
 
     rep_data = [[company], [], ['Компания', 'Сотрудник', 'Номер карты', 'Тариф', 'Время въезда', 'Время выезда', 'Длительность', 'Стоимость']]
     t = [list(row) + [row.TimeExit - row.TimeEntry] + [ba.bill_daily_list(datetime.datetime.strftime(row.TimeEntry, date_frm),
